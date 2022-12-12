e82ee5521de55843766da32d9438c418e0e2d711
athulya2000 <athulyav00@gmail.com>
2022-12-12 12:36:28 +0530

    search employee

--- src/main/java/com/example/NestDigitalApp_Backend/dao/EmployeeDao.java
+++ src/main/java/com/example/NestDigitalApp_Backend/dao/EmployeeDao.java
@@ -1,7 +1,14 @@
 package com.example.NestDigitalApp_Backend.dao;
 
 import com.example.NestDigitalApp_Backend.model.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+
+    @Query(value="SELECT `id`, `designation`, `emailid`, `employeecode`, `employeename`, `password`, `phoneno`, `salary`, `username` FROM `employee` WHERE `employeecode`= :employeecode",nativeQuery = true)
+    List<Employee> SearchEmployee(@Param("employeecode") String employeecode);
 }
